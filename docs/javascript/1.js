cdae5fa2d32ee0a433179654f5a3c5d13b20ccd9
tammy3638 <1208974791@qq.com>
2021-06-22 22:46:23 +0800

    ts

---
+++ docs/javascript/1.js
@@ -0,0 +1,7 @@
+﻿<script>
+    var link=document.getElementById("link");//获取a对象
+    breadcrumb.onclick=function(){//a 标签点击时调用的函数
+        this.parentNode.style.backgroundColor="#f00";//a标签的父级元素即div改变背景颜色
+        return false;//取消a标签的默认跳转方式
+    }
+</script>
